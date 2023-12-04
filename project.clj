0b808986eaae8caa01a2fefcdc8f1a1a24890858
Mourjo Sen <hello@mourjo.me>
2023-12-04 06:57:08 +0100

    Bump to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.mourjo/dynamic-redef "0.1.0"
+(defproject me.mourjo/dynamic-redef "0.1.1"
   :description "Dynamically redefine Clojure functions"
   :url "https://medium.com/helpshift-engineering/a-study-in-parallelising-tests-b5253817beae"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
